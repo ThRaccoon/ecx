a24729e328fd82ba4b52cdc00d542f4ede5e1893
Ivan Stoyanov <thraccoon04@gmail.com>
2026-08-13 04:42:56 -0400

    (tests): refactor StorageTests

--- tests/containers/storage_tests.cpp
+++ tests/containers/storage_tests.cpp
@@ -11,10 +11,17 @@ using ecx::internal::Storage;
 namespace
 {
     template <typename T>
+    class StorageAccessor : public Storage<T, int>
+    {
+    public:
+        using Storage<T, int>::on_erase;
+    };
+
+    template <typename T>
     class StorageFixture : public ::testing::Test
     {
     public:
-        Storage<T, int> storage;
+        StorageAccessor<T> storage;
     };
 
     using MyTypes = ::testing::Types<uint32_t, uint64_t>;
@@ -25,9 +32,9 @@ namespace
 // insert
 //
 
-TYPED_TEST(StorageFixture, InsertAddsElementToElements)
+TYPED_TEST(StorageFixture, InsertMakesElementIterable)
 {
-    this->storage.insert(1, 23);
+    this->storage.insert(1u, 23);
 
     std::vector<int> result(this->storage.begin(), this->storage.end());
 
@@ -38,26 +45,34 @@ TYPED_TEST(StorageFixture, InsertAddsElementToElements)
 // get
 //
 
-TYPED_TEST(StorageFixture, GetExistingHandleReturnsElement)
+TYPED_TEST(StorageFixture, GetReturnsElement)
 {
-    this->storage.insert(1, 23);
+    this->storage.insert(1u, 23);
 
-    EXPECT_EQ(this->storage.get(1), 23);
+    EXPECT_EQ(this->storage.get(1u), 23);
 }
 
-TYPED_TEST(StorageFixture, GetNonExistingHandleAsserts)
+TYPED_TEST(StorageFixture, GetAssertsOnNonExistentElement)
 {
-    EXPECT_DEATH(this->storage.get(1), "");
+    EXPECT_DEATH(this->storage.get(1u), "");
+}
+
+TYPED_TEST(StorageFixture, GetAssertsOnErasedElement)
+{
+    this->storage.insert(1u, 23);
+    this->storage.erase(1u);
+
+    EXPECT_DEATH(this->storage.get(1u), "");
 }
 
 //
 // clear
 //
 
-TYPED_TEST(StorageFixture, ClearEmptiesElements)
+TYPED_TEST(StorageFixture, ClearMakesElementsNotIterable)
 {
-    this->storage.insert(1, 23);
-    this->storage.insert(2, 34);
+    this->storage.insert(1u, 23);
+    this->storage.insert(2u, 34);
     this->storage.clear();
 
     std::vector<int> result(this->storage.begin(), this->storage.end());
@@ -69,10 +84,10 @@ TYPED_TEST(StorageFixture, ClearEmptiesElements)
 // reset
 //
 
-TYPED_TEST(StorageFixture, ResetEmptiesElements)
+TYPED_TEST(StorageFixture, ResetMakesElementsNotIterable)
 {
-    this->storage.insert(1, 23);
-    this->storage.insert(2, 34);
+    this->storage.insert(1u, 23);
+    this->storage.insert(2u, 34);
     this->storage.reset();
 
     std::vector<int> result(this->storage.begin(), this->storage.end());
@@ -81,27 +96,45 @@ TYPED_TEST(StorageFixture, ResetEmptiesElements)
 }
 
 //
+// begin / end
+//
+
+TYPED_TEST(StorageFixture, BeginEqualsEndWhenEmpty)
+{
+    EXPECT_EQ(this->storage.begin(), this->storage.end());
+}
+
+TYPED_TEST(StorageFixture, BeginIteratesAllElementsToEndWhenNotEmpty)
+{
+    this->storage.insert(1u, 23);
+    this->storage.insert(2u, 34);
+
+    std::vector<int> result(this->storage.begin(), this->storage.end());
+
+    EXPECT_EQ(result, (std::vector<int>{23, 34}));
+}
+
+//
 // on_erase
 //
 
-TYPED_TEST(StorageFixture, OnEraseRemovesElementFromElements)
+TYPED_TEST(StorageFixture, OnEraseMakesElementNotIterable)
 {
-    this->storage.insert(1, 23);
-    this->storage.erase(1);
+    this->storage.insert(1u, 23);
+    this->storage.erase(1u);
 
     std::vector<int> result(this->storage.begin(), this->storage.end());
 
     EXPECT_EQ(result, (std::vector<int>{}));
 }
 
-TYPED_TEST(StorageFixture, OnEraseNonLastElementCopiesLastIntoItsSlot)
+TYPED_TEST(StorageFixture, OnEraseMovesLastElementIntoErasedIndex)
 {
-    this->storage.insert(1, 23);
-    this->storage.insert(2, 34);
-    this->storage.insert(3, 45);
-    this->storage.erase(1);
+    this->storage.insert(1u, 23);
+    this->storage.insert(2u, 34);
+    this->storage.insert(3u, 45);
+    this->storage.erase(1u);
 
-    // In elements last (45) now sits where erased (23) was
     std::vector<int> result(this->storage.begin(), this->storage.end());
 
     EXPECT_EQ(result, (std::vector<int>{45, 34}));
